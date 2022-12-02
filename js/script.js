9b00159c1d80ec0c516de704ca7634f359f3774e
Gise21 <119279301+Gise21@users.noreply.github.com>
2022-12-02 03:57:19 -0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ function mostrar_menu(){
     document.getElementById("move-content").classList.toggle('move-container-all');
     document.getElementById("show-menu").classList.toggle('show-lateral');
 }
+
+document.addEventListener("keyup", e=>{
+
+    if (e.target.matches("#buscador")){
+
+        if (e.key ==="Escape")e.target.value = ""
+
+        document.querySelectorAll(".articulo").forEach(fruta =>{
+
+            fruta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?fruta.classList.remove("filtro")
+            :fruta.classList.add("filtro")
+        })
+    }
+})
